c92c7104f1524c986fe6470d97cfa468560dfe58
Chelsey Ingersoll <cjingers@asu.edu>
2023-03-30 23:03:28 +0000

    test and implementation for map calculate position

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -4,10 +4,10 @@ from levelup.position import Position
 
 
 class Direction(Enum):
-    NORTH = "n"
-    SOUTH = "s"
-    EAST = "e"
-    WEST = "w"
+    UP = "UP"
+    DOWN = "Down"
+    LEFT = "Left"
+    RIGHT = "Right"
 
 
 class GameMap:
@@ -27,12 +27,33 @@ class GameMap:
         self.position_count = len(self.positions)
 
     def is_valid_position(self, position: Position) -> bool:
-        pass
+        testX = position.coordinates[0]
+        testY = position.coordinates[1]
+        if testX > self.size[0] or testX < 1:
+            return False
+        if testY > self.size[1] or testY < 1:
+            return False
+        return True
 
     def calculate_position(
         self, starting_position: Position, direction: Direction
     ) -> Position:
-        pass
+        startX = starting_position.coordinates[0]
+        startY = starting_position.coordinates[1]
+
+        if direction is Direction.UP:
+            startY += 1
+        if direction is Direction.DOWN:
+            startY -= 1
+        if direction is Direction.RIGHT:
+            startX += 1
+        if direction is Direction.LEFT:
+            startX -= 1
+
+        if self.is_valid_position(Position(startX, startY)):
+            return Position(startX, startY)
+        else:
+            return starting_position
 
     def get_total_positions(self) -> int:
         return self.size[0] * self.size[1]
--- tests/test_map.py
+++ tests/test_map.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from levelup.map import GameMap
-
+from levelup.position import Position
+from levelup.map import Direction
 
 class TestMap(TestCase):
     def test_init(self):
@@ -12,4 +13,19 @@ class TestMap(TestCase):
         pos = testMap.get_positions()
         self.assertEqual(len(pos), 100)
 
+    def test_calculate_position(self):
+        tPos = Position(5,5)
+        tDirection = Direction.UP
+        tWorld = GameMap()
+        newPosition = tWorld.calculate_position(tPos, tDirection)
+        self.assertEqual(newPosition.coordinates[0], 5)
+        self.assertEqual(newPosition.coordinates[1], 6)
+
+    def test_calculate_position(self):
+        tPos = Position(10,10)
+        tDirection = Direction.UP
+        tWorld = GameMap()
+        newPosition = tWorld.calculate_position(tPos, tDirection)
+        self.assertEqual(newPosition.coordinates[0], 10)
+        self.assertEqual(newPosition.coordinates[1], 10)
         
