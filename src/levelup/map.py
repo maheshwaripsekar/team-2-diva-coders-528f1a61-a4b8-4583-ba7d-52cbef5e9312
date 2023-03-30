70a3f4f201f41fc5fd100a5c7d50ac442b75caa1
Chelsey Ingersoll <cjingers@asu.edu>
2023-03-30 22:10:47 +0000

    Added test and implementation for GameMap get_positions

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -11,16 +11,20 @@ class Direction(Enum):
 
 
 class GameMap:
-    starting_position: Position = Position(0, 0)
+    starting_position: Position = Position(5, 5)
     size: Tuple[int, int] = (10, 10)
     position_count: int
     positions: List[Position]
 
     def __init__(self):
+        self.positions = list()
         self.create_positions()
 
     def create_positions(self) -> None:
-        pass
+        for x in list(range(0, self.size[0])):
+            for y in list(range(0, self.size[1])):
+                self.positions.append(Position(x, y))
+        self.position_count = len(self.positions)
 
     def is_valid_position(self, position: Position) -> bool:
         pass
@@ -32,3 +36,6 @@ class GameMap:
 
     def get_total_positions(self) -> int:
         return self.size[0] * self.size[1]
+
+    def get_positions(self) -> List:
+        return self.positions
--- tests/test_map.py
+++ tests/test_map.py
@@ -6,3 +6,10 @@ class TestMap(TestCase):
     def test_init(self):
         testWorld = GameMap()
         self.assertEqual(testWorld.get_total_positions(), 100)
+
+    def test_get_positions(self):
+        testMap = GameMap()
+        pos = testMap.get_positions()
+        self.assertEqual(len(pos), 100)
+
+        
