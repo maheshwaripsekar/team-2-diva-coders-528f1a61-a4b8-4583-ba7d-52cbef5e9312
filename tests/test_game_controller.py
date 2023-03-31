73ad2d27a55a414ab7881ed1cb7d6645d01b55cc
Chelsey Ingersoll <cjingers@asu.edu>
2023-03-31 18:33:20 +0000

    test and implementation of GameController setCharacterPosition

--- tests/test_game_controller.py
+++ tests/test_game_controller.py
@@ -4,7 +4,7 @@ from levelup.controller import GameController, CharacterNotFoundException
 from levelup.character import DEFAULT_CHARACTER_NAME, InvalidMoveException, Character
 from levelup.map import Direction
 from levelup.position import Position
-
+from typing import Tuple
 
 class TestGameController(TestCase):
     def test_init(self):
@@ -32,3 +32,19 @@ class TestGameController(TestCase):
         testStatus = testGame.get_status()
         self.assertEqual(testStatus.character_name, "Bob the Builder")
         self.assertIsNotNone(testStatus.current_position)
+
+    def test_total_positions(self):
+        testGame = GameController()
+        testGame.create_character("Morgan Freeman")
+        testGame.start_game()
+        totalPositions = testGame.get_total_positions()
+        self.assertEqual(100, totalPositions)
+
+    def test_set_character_position(self):
+        testGame = GameController()
+        testGame.create_character("Edward James Olmos")
+        testGame.start_game()
+        testGame.set_character_position(tuple((3,4)))
+        newPosition = testGame.character.get_position()
+        self.assertEqual(newPosition[0], 3)
+        self.assertEqual(newPosition[1], 4)
